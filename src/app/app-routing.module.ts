462c195e34e430400cc78aac37737df9472ea437
Camila <camilanobre170@gmail.com>
2019-09-23 09:11:53 -0300

    finalização página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
+  }, 
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  },
+  }, 
   { path: 'modal-sobre', loadChildren: './modal-sobre/modal-sobre.module#ModalSobrePageModule' },
   { path: 'modal-duvidas', loadChildren: './modal-duvidas/modal-duvidas.module#ModalDuvidasPageModule' },
   { path: 'caso-suspeito', loadChildren: './caso-suspeito/caso-suspeito.module#CasoSuspeitoPageModule' },
--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  authForm: FormGroup;
+  configs = {
+    isSignIn: true,
+    action: 'Entrar',
+    actionChange: 'Criar Conta'
+  };
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(8)])
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  get email(): FormControl {
+    return <FormControl>this.authForm.get('email')
+  }
+  get senha(): FormControl {
+    return <FormControl>this.authForm.get('senha')
+  }
+  get name(): FormControl {
+    return <FormControl>this.authForm.get('name')
+  }
+
+  changeAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Entrar' : 'Criar Conta';
+    this.configs.actionChange = isSignIn ? 'Criar Conta' : 'Já tenho uma conta!';
+    !isSignIn 
+      ? this.authForm.addControl('name', this.nameControl) 
+      : this.authForm.removeControl('name')
+  }
+
+  onSubmit(): void {
+    console.log('Dados de Formulario', this.authForm.value)
   }
 
 }

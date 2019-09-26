bbbe1164b127edcf93e296559f1af0a9b72f9583
Camila <camilanobre170@gmail.com>
2019-09-26 07:29:54 -0300

    finalizando os guardiões de rota

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,17 +1,27 @@
 import { AuthService } from './../services/auth.service';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router, CanActivate } from '@angular/router';
+import {  ActivatedRouteSnapshot, CanActivate, 
+          CanActivateChild, CanLoad, Route, Router, 
+          RouterStateSnapshot, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
   constructor(private authService: AuthService,
               private router: Router) {}
 
-canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  return this.checkAutState(state.url);
+}
+
+canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  return this.canActivate(route, state);
+}
+
+canLoad(route: Route, segments: UrlSegment[]): Observable<boolean>
 
   private checkAutState(redirect: string): Observable<boolean>{
     return this.authService.isAuthenticated.pipe(

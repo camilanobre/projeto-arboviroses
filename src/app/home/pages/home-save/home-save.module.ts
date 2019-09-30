d5a5afdbd25af6b23e2392e0c0de88299bb0b92f
Camila <camilanobre170@gmail.com>
2019-09-30 16:59:57 -0300

    css

---
+++ src/app/home/pages/home-save/home-save.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { HomeSavePage } from './home-save.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeSavePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [HomeSavePage]
+})
+export class HomeSavePageModule {}
---
+++ src/app/home/pages/home-save/home-save.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-save',
+  templateUrl: './home-save.page.html',
+  styleUrls: ['./home-save.page.scss'],
+})
+export class HomeSavePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
